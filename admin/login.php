8eeb38e809d667c6838f5b0230117da70d006396
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-04-19 23:31:00 -0500

    Login de administrador

---
+++ admin/login.php
@@ -0,0 +1,23 @@
+<?php
+$user = $_POST["inpUser"];
+$pass = $_POST["inpPass"];
+
+$enPass = sha1($pass);
+
+require "../scripts/service/connection.php";
+
+$query = "SELECT * FROM pass WHERE id = '$user' AND pwd = '$enPass'";
+$consulta = mysqli_query($connection, $query);
+$u = mysqli_fetch_array($consulta, MYSQLI_NUM);
+
+if(count($u) > 0)
+{
+    session_start();
+    if($_SESSION["activeSession"] == true)
+    {
+        session_unset();
+    }
+    $_SESSION["activeSession"] = true;
+    header("Location:index.php");
+}
+?>
