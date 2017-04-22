5957411aacba880c9892b46c1d5e159f3818a1a9
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-04-22 13:49:58 -0500

    Issue 7

---
+++ admin/eventos/detalles.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+if($_SESSION["activeSession"] = true)
+{
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Evento</title>
+    </head>
+
+    <body>
+        <h3>Abrir en:</h3>
+        <ul>
+            <li>Microsoft edge</li>
+            <li>Google chrome</li>
+            <li>Opera (Escritorio)</li>
+            <li>Safari...?</li>
+        </ul>
+
+        <form>
+            <!--Esta página servirá como plantilla tanto para agregar como para modificar eventos-->
+            <label>Nombre de evento</label>
+            <input type="text" placeholder="Nombre de evento" name="evName"><br>
+            <label>Fecha y hora de evento</label>
+            <input type="datetime-local" ame="evDateTime"><br>
+            <label>Canal del evento</label>
+            <!--Llenar con los canales cargados desde la base de datos-->
+            <select id="slnChannel" namespace="evChannel" placeholder="Canal">
+            </select><br>
+            <label>Detalles del evento</label>
+            <input type="textarea" placeholder="Detalles del evento" name="evDescription"><br>
+            <input type="submit" value="Aceptar" />
+        </form>
+    </body>
+</html>
+<?php
+}
+else {
+    header("Location:login.html");
+}
+?>
