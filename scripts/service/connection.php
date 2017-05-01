d139d77111ad2fab3eccbcde82812c6eca7768e1
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-05-01 00:44:15 -0500

    Conexión con base de datos en línea

--- scripts/service/connection.php
+++ scripts/service/connection.php
@@ -1,6 +1,11 @@
 <?php
-    //Cambiar en pruebas
-    //$connection = mysqli_connect("mysql.hostinger.mx", "u331700213_usr", "holamundo", "u331700213_broad")
     //host, user, password, database
-    $connection = mysqli_connect("localhost", "root", "", "broadcaster")
+    // Original
+    //$connection = mysqli_connect("mysql.hostinger.mx", "u331700213_usr", "holamundo", "u331700213_broad")
+
+    // Pruebas (En línea)
+    $connection = mysqli_connect("sql5.freemysqlhosting.net", "sql5120931", "utfc1uHGRz", "sql5120931")
+
+    // Local (Únicamente para pruebas sin conexión)
+    //$connection = mysqli_connect("localhost", "root", "", "broadcaster")
 ?>
