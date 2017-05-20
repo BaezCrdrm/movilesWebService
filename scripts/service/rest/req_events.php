153a5b8660145cd7a64acfa22d730574f470add7
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-05-20 02:44:10 -0500

    Cambia datos req_eventos

--- scripts/service/rest/req_events.php
+++ scripts/service/rest/req_events.php
@@ -4,10 +4,10 @@ ini_set('display_errors', 0);
 $id = $_REQUEST["evId"];
 $tp = $_REQUEST["tpId"];
 require "../queries.php";
-$query = "SELECT event.ev_id, event.ev_name, event.ev_sch";
+$query = "SELECT event.ev_id, event.ev_name, event.ev_sch, event.ev_des";
 if($id != null && $id != "null")
 {
-	$query .= ", types.tp_name, types.tp_img FROM event INNER JOIN type_event ";
+	$query .= ", types.tp_id, types.tp_name FROM event INNER JOIN type_event ";
 	$query .= "ON type_event.ev_id = event.ev_id INNER JOIN types ON type_event.tp_id = types.tp_id ";
 	$query .= "WHERE event.ev_id = '$id'";
 }
