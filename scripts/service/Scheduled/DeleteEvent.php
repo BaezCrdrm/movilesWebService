71072664e6a6f3e570baf438a5306037d4f83edf
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-06-10 16:09:08 -0500

    Creación de script - Issue 21

---
+++ scripts/service/Scheduled/DeleteEvent.php
@@ -0,0 +1,20 @@
+<?php
+require "queries.php";
+$query = "SELECT ev_id, ev_sch_end FROM event";
+
+$res = executeQuery($query);
+$now = date("Y-m-d H:i:s");
+
+foreach ($res as $row) {
+    if($now >= $row['ev_sch_end'])
+    {
+        $query = "DELETE FROM event WHERE ev_id = '".$row['ev_id']."'"; 
+        if(executeQuery($query)) {
+            echo "The event has been succesfully deleted. Id: ".$row['ev_id'];
+        }
+        else {
+            echo "There has been a problem while deleting the event. Id: ".$row['ev_id'];
+        }
+    }
+}
+?>
