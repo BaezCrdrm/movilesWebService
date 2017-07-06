9d587ec1b22ecb288604d09e77a8ceed89c5d76a
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-07-05 20:10:22 -0500

    Info

--- scripts/service/connection.php
+++ scripts/service/connection.php
@@ -1,10 +1,11 @@
 <?php
+// MySQL
     //host, user, password, database
     /// Original
     //$connection = mysqli_connect("mysql.hostinger.mx", "u331700213_usr", "holamundo", "u331700213_broad")
 
     /// Pruebas (En línea)
-    $connection = mysqli_connect("sql5.freemysqlhosting.net", "sql5120931", "utfc1uHGRz", "sql5120931")
+    $connection = mysqli_connect("sql5.freemysqlhosting.net", "sql5120931", "utfc1uHGRz", "sql5120931");
 
     /// Local (Únicamente para pruebas sin conexión)
     //$connection = mysqli_connect("localhost", "root", "", "broadcaster")
